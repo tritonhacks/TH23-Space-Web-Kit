f7935ef319f0787ee7b5840a50ce4fee614f79f8
Jeff Xu <jjx003@ucsd.edu>
2022-02-03 16:02:41 -0800

    Update UI, add autocomplete

--- main.py
+++ main.py
@@ -10,10 +10,33 @@ app = FastAPI()
 app.mount("/home", StaticFiles(directory="static"), name="static")
 fish_watch_api = 'https://www.fishwatch.gov/api/species/'
 
+FISH_NAMES = []
+
 
 @app.get("/")
 def read_root():
     return RedirectResponse(url="/home/index.html")
+
+
+@app.get("/names")
+def read_names():
+    global FISH_NAMES
+
+    if len(FISH_NAMES) > 0:
+        return FISH_NAMES
+
+    res = requests.get(fish_watch_api)
+    if not res.ok:
+        raise HTTPException(status_code=404, detail=res.reason)
+
+    decoded = res.json()
+    names = []
+    for entry in decoded:
+        names.append(entry["Species Name"])
+
+    FISH_NAMES = names
+
+    return names
 
 
 @app.get("/search/{name}")
@@ -23,12 +46,13 @@ def read_item(name: Optional[str] = ""):
         raise HTTPException(status_code=404, detail=res.reason)
 
     decoded = res.json()
-    
+
     if len(decoded) == 0:
         # TODO: Fix statuscode for no results found
         raise HTTPException(status_code=404, detail="No results found")
 
     return decoded
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=5000, log_level="info")
--- static/main.js
+++ static/main.js
@@ -3,37 +3,63 @@ const fishwatchEndpoint = 'http://127.0.0.1:5000/search/'
 // Use this to load in auto-complete results
 document.addEventListener("DOMContentLoaded", async function () {
     let searchButton = document.getElementById("submit")
-    
-    searchButton.addEventListener("click", async function(event) {
+    let resultsDiv = document.getElementById("results")
+    let resultsDisplay = document.getElementById('result-text');
+    let resultsBioDisplay = document.getElementById('result-bio-text');
+    let resultsHeader = document.getElementById('result-header');
+    let resultLocation = document.getElementById("result-location");
+    let imageDisplay = document.getElementById("result-image");
+    let queryArea = document.getElementById('query');
+    let dataList = document.getElementById('fish-names')
+
+    let results = await fetch('http://127.0.0.1:5000/names', {
+        method: 'GET',
+        headers: { 'Accept': 'application/json' }
+    });
+
+    let decoded = (await results.json());
+
+    for (var i = 0; i < decoded.length; i++) {
+        let option = document.createElement("option")
+        option.value = decoded[i]
+        dataList.appendChild(option)
+    }
+
+    resultsDiv.style.display = 'none'
+
+    searchButton.addEventListener("click", async function (event) {
         event.preventDefault() // Prevent page redirection
-        
-        let fish = document.getElementById('query').value;
-        let resultsDisplay = document.getElementById('result-text');
-        let imageDisplay = document.getElementById("result-image");
-        
+
+        let fish = queryArea.value
         let results = await fetch(fishwatchEndpoint + fish, {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            }
+            headers: { 'Accept': 'application/json' }
         });
 
         if (!results.ok) {
-            resultsDisplay.innerHTML = `Error, ${fish} not found`
+            resultsDiv.style.display = ''
+            resultsHeader.innerHTML = `ERROR: "${fish}" not found`
             return
         }
 
+        resultsDiv.style.display = ''
+
         // Fishing Rate, Habitat, Location, Population, Species Illustration Photo >src, Scientific Name
-        let decoded = await results.json()
-        console.log(decoded)
-        let description = decoded[0]["Physical Description"]
+        let decoded = (await results.json())[0]
+        let description = decoded["Physical Description"]
 
         // get random image from image gallery
-        let imageURL = decoded[0]["Image Gallery"][Math.floor(Math.random() * decoded[0]["Image Gallery"].length)].src
-        
+        let images = decoded["Image Gallery"]
+
+        console.log(images)
+        console.log(decoded)
+        let imageURL = images[Math.floor(Math.random() * images.length)].src
+
         imageDisplay.src = `${imageURL}`
         resultsDisplay.innerHTML = description;
+        resultsHeader.innerHTML = decoded["Species Name"]
+        resultLocation.innerHTML = decoded["Location"]
+        resultsBioDisplay.innerHTML = decoded["Biology"]
     })
 })
 
